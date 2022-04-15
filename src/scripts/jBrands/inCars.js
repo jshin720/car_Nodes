5e4aa2b0b3277508dcc2b0554db5263d2942066c
Jshin720 <jeffshin720@gmail.com>
2022-04-15 18:25:13 -0400

    added japan car brands:

---
+++ src/scripts/jBrands/inCars.js
@@ -0,0 +1,44 @@
+const inCars = {
+  q50: {
+    name: "Q50",
+    price: "$42,100",
+    type: "Sedan",
+    engine: "3.0-liter V6 twin turbo 300 HP",
+    gasMileage: "20 / 29 / 23",
+    description: "Q50 has a tendency to move people. Meticulous design inside and out sets a high bar for what a luxury sedan could be, and shatters it. It's made to arrive in elegance, every time"
+  },
+  q60: {
+    name: "Q60",
+    price: "$41,750",
+    type: "coupe",
+    engine: "3.0-liter V6 twin turbo 300 HP or 3.0-liter V6 twin turbo 400 HP",
+    gasMileage: "19 / 28 / 22",
+    description: "Engineered with runaway ambition. Designed with runway style. The Q60 luxury coupe was born out of a labor of love, with appeal that deeply resonates on every level for every passenger."
+  },
+  qx50: {
+    name: "Qx50",
+    price: "$39,150",
+    type: "SUV",
+    engine: "2.0L VC-Turbo 4-cylinder engine (Variable Compression Turbo",
+    gasMileage: "23/29/26 AWD",
+    description: "This is innovation, refined for those who demand more. This is style, reinvented for those who defy the status quo. This is luxury, reimagined around you and designed to be lived in. This is the revolutionary 2022 INFINITI QX50."
+  },
+  qx55: {
+    name: "Qx55",
+    price: "$46,500",
+    type: "SUV",
+    engine: "2.0L VC-Turbo 4-cylinder engine (Variable Compression Turbo)",
+    gasMileage: "22 / 28 ",
+    description: "This isn\’t a luxury crossover for just anyone. But then, you\’re not just anyone \– and this isn\’t just any crossover. Introducing INFINITI\’s long-anticipated return to the crossover coupe segment it created two decades ago: the uniquely provocative all-new 2022 INFINITI QX55."
+  },
+  qx60: {
+    name: "Qx60",
+    price: "$46,850",
+    type: "SUV",
+    engine: "3.5 liter V6 direct-injection engine",
+    gasMileage: "21 / 26 / 23(2WD) or 20 / 25 / 22(AWD)",
+    description: "Take a moment to picture the idea of luxury in your mind\’s eye. We imagine it to be commanding. Confidence-inspiring. Elegant and sophisticated. We imagine it to be the all-new 2022 INFINITI QX60\–completely re-designed to help you take on life in style This is luxury, re-imagined."
+  }
+}
+
+export default inCars;
---
+++ src/scripts/jBrands/toCars.js
@@ -0,0 +1,52 @@
+const toCars = {
+  prius: {
+    name: "Prius Prime",
+    price: "$28,220",
+    type: "Hybrid",
+    engine: "1.8L 4-Cyl. ECVT Hybrid",
+    gasMileage: "55/53/54 MPG",
+    description: "Prius Prime is one of our most advanced hybrids yet."
+  },
+  camry: {
+    name: "Camry",
+    price: "$25,395",
+    type: "Sedan",
+    engine: "208 Hybrid Net System Power 2.5L 4-Cylinder ECVT",
+    gasMileage: "44/47/46",
+    description: "The powerful, stylish 2022 Camry Hybrid gives up nothing with its optimized fuel economy, * advanced tech and stirring drive. We have expanded our hybrid battery warranty to reflect our confidence in the quality, dependability and reliability of our products. From the 2020 model year forward, every Toyota hybrid battery warranty will cover 10 years from date of first use or 150,000 miles, whichever comes first (previously 8 years or 100,000 miles). "
+  },
+  mirai: {
+    name: "Mirai",
+    price: "$49,500",
+    type: "Electric",
+    engine: "Fuel Cell Electric",
+    gasMileage: "67/64/65",
+    description: "At the core of Mirai, hydrogen from the fuel tank and air entering from the intake grille meet in the Fuel Cell Stack. There, a chemical reaction involving the oxygen in the air and hydrogen creates electricity—powering Mirai. In the end, the only by-product is water."
+  },
+  highlander: {
+    name: "Highlander",
+    price: "$35,405",
+    type: "SUV",
+    engine: "3.5L V6 8AT (AWD)",
+    gasMileage: "20/27/23",
+    description: "2022 Highlander Designed to be outstanding."
+  },
+  b2: {
+    name: "bZ4X",
+    price: "$42,000",
+    type: "Electric",
+    engine: "Electric",
+    gasMileage: "242 mileage range",
+    description: "The All-New 2023 bZ4X"
+  },
+  Supra: {
+    name: "Supra",
+    price: "$43,290",
+    type: "Coupe",
+    engine: "3.0L Inline 6-Cylinder 8-Speed Automatic",
+    gasMileage: "22/30/25",
+    description: "Producing 382 horsepower * with an impressive 368 lb.-ft. of torque, this engine gives you plenty of power and instant response."
+  }
+}
+
+export default toCars;
