0fc233c5a2fe18053ddac3f4af25e560357c34e9
Jshin720 <jeffshin720@gmail.com>
2022-04-15 19:19:45 -0400

    finished french car brands

---
+++ src/scripts/gBrands/bmwCars.js
@@ -0,0 +1,52 @@
+const renCars = {
+  accord: {
+    name: "Accord",
+    price: "$27,135",
+    type: "Sedan",
+    engine: "4-cyl 1.5T,  4-cyl 2.0T or hybrid",
+    gasMileage: "30 / 38 / 33 or 22 / 32 / 26 or 48 / 47 / 47",
+    description: "With three advanced powertrains, a cabin full of technological innovations and Honda Sensing® standard on all trims, the Accord is everything a sedan should be and more."
+  },
+  civc: {
+    name: "Civic",
+    price: "$27,300",
+    type: "Sedan",
+    engine: "In-Line 4-Cylinder with Turbocharger",
+    gasMileage: "27 / 37 / 31",
+    description: "The Civic ran away with this comparison.It outclasses the Corolla in almost every way we can think of.”—MotorTrend"
+  },
+  hrv: {
+    name: "HRV",
+    price: "$21,870",
+    type: "SUV",
+    engine: "In-Line 4-Cylinder",
+    gasMileage: "28/34/30 (CVT)2WD or 26/31/28 (CVT)AWD",
+    description: "The 2022 Honda HR-V will point you toward adventure. With distinct exterior lines and great interior features, this subcompact SUV is comfortable and cool."
+  },
+  crv: {
+    name: "CRV",
+    price: "$26,400",
+    type: "SUV",
+    engine: "In-Line 4-Cylinder with Turbocharger",
+    gasMileage: "28 / 34 / 30",
+    description: "Seek out the road less traveled in the 2022 Honda CR-V. This SUV offers stylish trim options, an available hybrid powertrain and top-rated safety features."
+  },
+  pilot: {
+    name: "Pilot",
+    price: "$37,580",
+    type: "SUV",
+    engine: "V6",
+    gasMileage: "20 / 27 / 23(2WD) or 19 / 26 / 22(AWD)",
+    description: "With three advanced powertrains, a cabin full of technological innovations and Honda Sensing® standard on all trims, the Accord is everything a sedan should be and more."
+  },
+  clarity: {
+    name: "Clarity",
+    price: "$27,135",
+    type: "Sedan",
+    engine: "4-cyl 1.5T,  4-cyl 2.0T or hybrid",
+    gasMileage: "30 / 38 / 33 or 22 / 32 / 26 or 48 / 47 / 47",
+    description: "The 2022 Honda Pilot provides refuge for folks who put a premium on practicality but aren't too keen on the minivan lifestyle."
+  }
+}
+
+export default renCars;
---
+++ src/scripts/gBrands/mbCars.js
@@ -0,0 +1,45 @@
+const acCars = {
+  ilx: {
+    name: "ILX",
+    price: "$27,300",
+    type: "Sedan",
+    engine: "Aluminum-Alloy 4-Cylinder 2.4L",
+    gasMileage: "24 / 33 / 27",
+    description: "Light on its feet, the ILX meets your every impulse. With its energetic 201-HP82 i-VTEC® engine mated to a quick shifting 8-speed Dual Clutch Transmission (DCT), you\’re up for an invigorating driving experience that leaves the conventional at the curb."
+  },
+  tlx: {
+    name: "TLX",
+    price: "$38,500",
+    type: "Sedan",
+    engine: "Turbo Aluminum-Alloy 4-Cylinder or Turbo Aluminum-Alloy 6-Cylinder",
+    gasMileage: "21 / 29 / 24 or 19 / 24 / 21",
+    description: "The 2022 TLX pushes the limits of performance, tech, and design. The TLX is ready with an exhilarating 272-HP108 VTEC® Turbo engine that befits the premium amenities and alluring, sculpted body. Step up to Type S levels of performance and experience the most powerful Acura sedan ever built. A 355-HP126 3.0L Turbo V6, rear-biased Super Handling All-Wheel Drive™ (SH-AWD®), and a bespoke Sport+ mode deliver pleasure and power like never before."
+  },
+  rdx: {
+    name: "RDX",
+    price: "$40,100",
+    type: "SUV",
+    engine: "Turbo Aluminum-Alloy 4-Cylinder",
+    gasMileage: "22 / 28 / 24(2WD) or 21 / 27 / 23(AWD)",
+    description: "Feel your adrenaline flow with a hard-hitting 272-HP86 VTEC® Turbo engine and available Super Handling All-Wheel Drive™ that takes on corners with confidence. Available Adaptive Dampers and four IDS drive modes give you precision control over your ride."
+  },
+  mdx: {
+    name: "MDX",
+    price: "$48,000",
+    type: "SUV",
+    engine: "Turbo Aluminum-Alloy 6-Cylinder",
+    gasMileage: "17 / 21 / 19",
+    description: "The redesigned 2022 MDX houses a 290-HP81 V-6 engine paired to a double-wishbone front suspension for precision cornering. For even more exhilaration, the MDX Type S features an available 355-hp Turbo V6 engine and available air suspension that thrills with every drive. Handling is further enhanced by the Integrated Dynamics System, including a new Lift mode for Type S, while available wider 21-in alloy wheels ensure adrenaline-inducing performance."
+  },
+  nsx: {
+    name: "NSX",
+    price: "$169,500",
+    type: "Super Car",
+    engine: "3.5 L V6",
+    gasMileage: "21 city / 22 highway",
+    description: "Hand-built by master technicians at the Performance Manufacturing Center (PMC) with a limited production of 350 units globally, the new NSX embodies the pinnacle of dynamic driving worthy of the Type S badge. While continuing to honor its lineage with meticulous engineering and striking design, the NSX Type S pushes the boundaries of possibility with more power and more sound for a more adrenaline-fueled drive."
+  }
+
+}
+
+export default acCars;
