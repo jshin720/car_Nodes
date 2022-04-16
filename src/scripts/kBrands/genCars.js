df273601fab32477d20b6aa5e4c9fa039eb3653a
Jshin720 <jeffshin720@gmail.com>
2022-04-16 11:39:51 -0400

    finished korean cars

---
+++ src/scripts/kBrands/genCars.js
@@ -0,0 +1,52 @@
+const genCars = {
+  gen1: {
+    name: "G70",
+    price: "$44,450",
+    type: "Sedan",
+    engine: "3.3-liter Twin Turbocharged V6",
+    gasMileage: "17/25 City/Hwy",
+    description: "Every drive is exhilarating in the redesigned G70, with unmistakable styling and advanced safety backed by an IIHS TOP SAFETY PICK+* rating for vehicles built after June 2021. Unleash a refined performance that delivers more punch off the line, an intoxicating growl, and an overall thrilling experience."
+  },
+  gen2: {
+    name: "G80",
+    price: "$63,700",
+    type: "Sedan",
+    engine: "3.5-Liter Twin- Turbocharged V6",
+    gasMileage: "17/26 city/hwy",
+    description: "Delivering a cutting-edge driving experience, intuitive technology and ambitious design, the reimagined G80 has been lauded as everything a midsize luxury sedan should be—and so much more."
+  },
+  gen3: {
+    name: "G90",
+    price: "$81,200",
+    type: "Sedan",
+    engine: "5.0L V8 Automatic Transmission AWD",
+    gasMileage: "16/23 City/Hwy",
+    description: "A bold front end and premium styling make an unmistakable first impression; optimal handling and uncompromising comfort leave a lasting one. The world-class Genesis G90 stands out among a family of showstoppers, exemplifying a dedication to ingenuity through high-quality design and innovative technologies.."
+  },
+  gen4: {
+    name: "GV60",
+    price: "$55,000 est",
+    type: "Electric SUV",
+    engine: "dual-motor all-wheel-drive",
+    gasMileage: "249 miles per charge",
+    description: "The first-ever Genesis GV60 SUV is here to electrify expectations. With a dynamic design, up to an EPA-estimated 248-mile range* and advanced technology inside and out, GV60 is undeniable proof that the future starts at Genesis. Be one of the first to experience GV60 by joining Genesis Priority One."
+  },
+  gen5: {
+    name: "GV70",
+    price: "$53,100",
+    type: "SUV",
+    engine: "3.5L Twin-turbocharged V6",
+    gasMileage: "19/25/21 city/ highway / combined",
+    description: "Named MotorTrend's 2022 SUV of the Year®, the first-ever GV70 is here to satisfy your cravings, needs and wants. Featuring incredible performance, athletic styling and advanced technology, this SUV proves that in the end, want wins."
+  },
+  gen6: {
+    name: "GV80",
+    price: "$77,200",
+    type: "SUV",
+    engine: "3.5-liter Twin Turbocharged V6",
+    gasMileage: "18/23/20 city/ highway / combined",
+    description: "Ruggedly luxurious, the 2022 GV80 pushes the limits of style and performance. With a full suite of intuitive technology, all-road capability, elevated comfort and showstopping design, the multifaceted SUV ushers in a new era of world-class adventuring. "
+  }
+}
+
+export default genCars;
